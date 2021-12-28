025a08b15e0281a3ba39de1d338b2cfe5b099155
tickstudiu <tickstudiu@gmail.com>
2021-12-28 12:00:34 +0700

    update base input component

--- components/bases/BaseInput.vue
+++ components/bases/BaseInput.vue
@@ -1,10 +1,32 @@
 <template>
-  <div>BaseInput</div>
+  <label :for="name">
+    {{ label }}
+    <input
+      :value="value" class="w-full appearance-none focus:outline-none border-b-2 focus:border-black py-1 px-2"
+      type="text" :name="name" @input="$emit('input', $event.target.value)"
+    />
+  </label>
 </template>
 
 <script>
-
 export default {
   name: 'BaseInput',
+
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+
+  computed: {
+    name() {
+      return this.label.toLowerCase();
+    },
+  },
 }
 </script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <h5>login</h5>
-    <BaseInput />
+    <BaseInput v-model="from.email" label="label"/>
+    <BaseInput v-model="from.password" label="label"/>
+    <button @click="handleLogin">submit</button>
   </div>
 </template>
 
@@ -9,5 +11,20 @@
 
 export default {
   name: 'LoginPage',
+
+  data() {
+    return {
+      from: {
+        email: 'te',
+        password: 'tes',
+      }
+    }
+  },
+
+  methods: {
+    handleLogin() {
+      console.log(this.from)
+    }
+  }
 }
 </script>
